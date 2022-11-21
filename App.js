f11a74dc64cc896afc1bd7fd6f5d8cbbbca74fcf
Kyra Henningson <kyrahenningson@hotmail.com>
2022-11-20 20:19:13 -0800

    DIG-1 * added ui component library

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Button, NativeBaseProvider } from "native-base";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Dig It!</Text>
+    <NativeBaseProvider style={styles.container}>
       <StatusBar style="auto" />
-    </View>
+      <Button>Dig it!</Button>
+    </NativeBaseProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
