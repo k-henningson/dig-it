bc091b7a87b91a39679c2da1ef088f0b3e449ecd
Kyra Henningson <kyrahenningson@hotmail.com>
2022-11-26 15:25:10 -0800

    DIG-17 * start basic layout of profile page

--- modules/screens/profile/ProfilePage.js
+++ modules/screens/profile/ProfilePage.js
@@ -1,12 +1,59 @@
+import { useState } from 'react';
 import { View } from 'react-native';
+import { HStack, Switch, Radio } from 'native-base';
 import StyledText from '../../components/StyledText';
 
 export default function ProfilePage() {
+    const [temperature, setTemp] = useState('Fahrenheit');
+    const [measurement, setMeasure] = useState('Imperial');
     return (
         <View
             style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
         >
-            <StyledText>Profile Page</StyledText>
+            <HStack alignItems="center" space={4}>
+                <StyledText>Novice</StyledText>
+                <Switch size="md" />
+                <StyledText>Pro</StyledText>
+            </HStack>
+            <Radio.Group
+                name="temperatureUnits"
+                accessibilityLabel="temperature units"
+                value={temperature}
+                onChange={(nextValue) => {
+                    setTemp(nextValue);
+                }}
+            >
+                <HStack alignItems="center" space={4}>
+                    <Radio value="Fahrenheit" my={1}>
+                        Fahrenheit
+                    </Radio>
+                    <Radio value="Celsius" my={1}>
+                        Celsius
+                    </Radio>
+                </HStack>
+            </Radio.Group>
+            <Radio.Group
+                name="measurementUnits"
+                accessibilityLabel="measurement units"
+                value={measurement}
+                onChange={(nextValue) => {
+                    setMeasure(nextValue);
+                }}
+            >
+                <HStack alignItems="center" space={4}>
+                    <Radio value="Imperial" my={1}>
+                        Imperial
+                    </Radio>
+                    <Radio value="Metric" my={1}>
+                        Metric
+                    </Radio>
+                </HStack>
+            </Radio.Group>
+            <HStack alignItems="center" space={4}>
+                <StyledText>Light 💡</StyledText>
+                <Switch size="md" />
+                <StyledText>Dark 🌚</StyledText>
+            </HStack>
         </View>
     );
 }
