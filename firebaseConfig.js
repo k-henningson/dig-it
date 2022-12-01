d28da7d7fa5ce8a53d02421d866485e0badeeda6
Kyra Henningson <kyrahenningson@hotmail.com>
2022-12-01 09:38:09 -0800

    DIG-2 * add firebase config file with unique identifiers * add metro config file

---
+++ firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: 'AIzaSyCTCtWdV9UQpUQcsVlzECWBYA2I_TLRRmc',
+    authDomain: 'dig-it-578c3.firebaseapp.com',
+    projectId: 'dig-it-578c3',
+    storageBucket: 'dig-it-578c3.appspot.com',
+    messagingSenderId: '342907405412',
+    appId: '1:342907405412:web:7942b6f02dbd9693ca4b71',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
