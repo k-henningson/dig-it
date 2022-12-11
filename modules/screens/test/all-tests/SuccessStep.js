f2023b926c3f89511fc8d620334ad63d45b35ad1
Kyra Henningson <kyrahenningson@hotmail.com>
2022-12-10 19:21:24 -0800

    DIG-30 * fix bug with success message so rerenders

--- modules/screens/test/all-tests/SuccessStep.js
+++ modules/screens/test/all-tests/SuccessStep.js
@@ -7,17 +7,20 @@ const successStepMessageOptions = [
     'Now get out and slay that pow!',
     "Let's go ski 🎶",
     'Smash those pillows!',
+    'Shred that pow 💥',
+    'Time to spread that gnar butter on the shred bread!',
 ];
 
-const randomMessage = Math.floor(
-    Math.random() * successStepMessageOptions.length
-);
+const getSuccessMessage = () => {
+    const index = Math.floor(Math.random() * successStepMessageOptions.length);
+    return successStepMessageOptions[index];
+};
 
 export default function SuccessStep() {
     return (
         <VStack alignItems="center" space={10} marginTop="30px">
             <StyledText>🎉</StyledText>
-            <StyledText>{successStepMessageOptions[randomMessage]}</StyledText>
+            <StyledText>{getSuccessMessage()}</StyledText>
         </VStack>
     );
 }
