464393c89ad7bd17a3c2400c26eefd22feedd3dd
Kyra Henningson <kyrahenningson@hotmail.com>
2022-12-13 14:51:31 -0800

    DIG-30 * add react native maps * render MapView with Vancouver as default location

--- modules/screens/test/all-tests/LocationStep.js
+++ modules/screens/test/all-tests/LocationStep.js
@@ -1,10 +1,28 @@
 import StyledText from '../../../components/StyledText/StyledText';
 import { VStack } from 'native-base';
+import MapView from 'react-native-maps';
+import { StyleSheet } from 'react-native';
 
 export default function LocationStep() {
     return (
         <VStack alignItems="center" space={10} marginTop="30px">
             <StyledText>Location Step</StyledText>
+            <MapView
+                style={styles.map}
+                initialRegion={{
+                    latitude: 49.2827,
+                    longitude: -123.116226,
+                    latitudeDelta: 0.0922,
+                    longitudeDelta: 0.0421,
+                }}
+            />
         </VStack>
     );
 }
+
+const styles = StyleSheet.create({
+    map: {
+        width: '100%',
+        height: '100%',
+    },
+});
