9ff5d7bc80e9d38a93ac9a005b63ec59aa0d6171
Kyra Henningson <kyrahenningson@hotmail.com>
2022-12-13 16:15:49 -0800

    DIG-30 * add marker

--- modules/screens/test/all-tests/LocationStep.js
+++ modules/screens/test/all-tests/LocationStep.js
@@ -1,21 +1,27 @@
 import StyledText from '../../../components/StyledText/StyledText';
 import { VStack } from 'native-base';
-import MapView from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
 import { StyleSheet } from 'react-native';
+
+const initialRegion = {
+    latitude: 49.2827,
+    longitude: -123.116226,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+};
 
 export default function LocationStep() {
     return (
-        <VStack alignItems="center" space={10} marginTop="30px">
+        <VStack alignItems="center" space={20} marginTop="30px">
             <StyledText>Location Step</StyledText>
-            <MapView
-                style={styles.map}
-                initialRegion={{
-                    latitude: 49.2827,
-                    longitude: -123.116226,
-                    latitudeDelta: 0.0922,
-                    longitudeDelta: 0.0421,
-                }}
-            />
+            <MapView style={styles.map} initialRegion={initialRegion}>
+                <Marker
+                    coordinate={{
+                        latitude: initialRegion.latitude,
+                        longitude: initialRegion.longitude,
+                    }}
+                ></Marker>
+            </MapView>
         </VStack>
     );
 }
