f50fcadab0035b24b0da490d2acf41572a0ed927
Kyra Henningson <kyrahenningson@hotmail.com>
2022-12-18 19:19:46 -0800

    DIG-38 * setup firestore * add useEffect to fetch testResult documents from firebase

--- modules/screens/history/HistoryPage.js
+++ modules/screens/history/HistoryPage.js
@@ -1,9 +1,20 @@
+import { useEffect } from 'react';
+import { collection, getDocs } from 'firebase/firestore';
 import { View } from 'react-native';
 import { HStack, ScrollView, Divider } from 'native-base';
+import { db } from './firebaseConfig';
 import StyledText from '../../components/StyledText/StyledText';
 import HistoryBox from './HistoryBox';
 
 export default function HistoryPage() {
+    useEffect(() => {
+        getDocs(collection(db, 'testResults')).then((res) => {
+            res.forEach((doc) => {
+                console.log(doc.data());
+            });
+        });
+    }, []);
+
     return (
         <ScrollView>
             <View
