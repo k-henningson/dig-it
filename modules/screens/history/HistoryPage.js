b3f35c9c589e9651a1a9e28d76911667dfb42cac
Kyra Henningson <kyrahenningson@hotmail.com>
2022-12-26 10:23:59 -0800

    DIG-39 * move formatDate helper function into utils folder

--- modules/screens/history/HistoryPage.js
+++ modules/screens/history/HistoryPage.js
@@ -6,6 +6,7 @@ import { useIsFocused } from '@react-navigation/native';
 import { db } from '../../../firebaseConfig';
 import StyledText from '../../components/StyledText/StyledText';
 import HistoryBox from './HistoryBox';
+import formatDate from '../../../commons/utils/formatDate-utils';
 
 export default function HistoryPage() {
     const [testResults, setTestResults] = useState([]);
@@ -22,42 +23,6 @@ export default function HistoryPage() {
             });
         }
     }, [isFocused]);
-    // console.log('testResults: ', testResults);
-
-    const formatDate = function (testResult) {
-        if (!testResult.timestamp) {
-            return `No date available`;
-        }
-        const dateObj = testResult.timestamp.toDate();
-        const day = dateObj.getDay();
-        const date = dateObj.getDate();
-        const month = dateObj.getMonth();
-        const year = dateObj.getFullYear();
-        const DAYS = [
-            'Sunday',
-            'Monday',
-            'Tuesday',
-            'Wednesday',
-            'Thursday',
-            'Friday',
-            'Saturday',
-        ];
-        const MONTHS = [
-            'January',
-            'February',
-            'March',
-            'April',
-            'May',
-            'June',
-            'July',
-            'August',
-            'September',
-            'October',
-            'November',
-            'December',
-        ];
-        return `${DAYS[day]}, ${date} ${MONTHS[month].slice(0, 3)} ${year}`;
-    };
 
     return (
         <ScrollView>
