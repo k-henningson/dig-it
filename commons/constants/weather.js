01ee3b870ca22de64b254474dc3491543ecd37a0
Kyra Henningson <kyrahenningson@hotmail.com>
2023-01-11 17:30:59 -0800

    DIG-45 * remove dead code from weather constant

--- commons/constants/weather.js
+++ commons/constants/weather.js
@@ -1,21 +1,3 @@
-// export const WEATHER_EMOJIS = {
-//     SUN: '☀️',
-//     LIGHT_CLOUDS: '🌤',
-//     CLOUDY: '☁️',
-//     RAIN: '🌧',
-//     FOG: '🌫',
-//     SNOW: '❄️',
-// };
-
-// export const weatherTestOptions = [
-//     { id: 'SUN', name: 'Sun', description: '☀️' },
-//     { id: 'LIGHT_CLOUDS', name: 'Light clouds', description: '🌤' },
-//     { id: 'CLOUDY', name: 'Cloudy', description: '☁️' },
-//     { id: 'RAIN', name: 'Rain', description: '🌧' },
-//     { id: 'FOG', name: 'Fog', description: '🌫' },
-//     { id: 'SNOW', name: 'Snow', description: '❄️' },
-// ];
-
 export const WEATHER_CONDITIONS = {
     SUN: {
         id: 'SUN',
