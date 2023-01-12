6dc8c427eca4bf0d165e489554c4a261bba0b32c
Kyra Henningson <kyrahenningson@hotmail.com>
2023-01-11 16:44:24 -0800

    DIG-45 * remove dead code

--- commons/constants/conditions.js
+++ commons/constants/conditions.js
@@ -1,23 +1,3 @@
-// export const SNOW_CONDITION_EMOJIS = {
-//     DUMPING: '💩',
-//     FRESH_POWDER: '💥',
-//     PACKED: '📦',
-//     GRANULAR: '🍚',
-//     WET_HEAVY: '💧',
-//     ICY: '🧊',
-//     THIN_COVER: '🧹',
-// };
-
-// export const snowConditionTestOptions = [
-//     { id: 'DUMPING', name: 'Dumping', description: '💩' },
-//     { id: 'FRESH_POWDER', name: 'Fresh Powder', description: '💥' },
-//     { id: 'PACKED', name: 'Packed', description: '📦' },
-//     { id: 'GRANULAR', name: 'Granular', description: '🍚' },
-//     { id: 'WET_HEAVY', name: 'Wet/Heavy', description: '💧' },
-//     { id: 'ICY', name: 'Icy', description: '🧊' },
-//     { id: 'THIN_COVER', name: 'Thin Cover', description: '🧹' },
-// ];
-
 export const SNOW_CONDITIONS = {
     DUMPING: {
         id: 'DUMPING',
