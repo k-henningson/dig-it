214b98c86ef9d903bbe17a7288f9cedd284fa26b
Kyra Henningson <kyrahenningson@hotmail.com>
2023-02-02 13:59:22 -0800

    DIG-48 * make number-utils tests * change date-utils duplicate test to alternate date string

--- commons/utils/numbers-utils.test.js
+++ commons/utils/numbers-utils.test.js
@@ -1,5 +1,30 @@
 import { generateNumberRange } from './numbers-utils';
 
 describe('generateNumberRange', () => {
-    it('', () => {});
+    it('returns an array', () => {
+        const generatedNumber = generateNumberRange({
+            start: undefined,
+            end: undefined,
+        });
+        expect(generatedNumber).toStrictEqual([]);
+    });
+
+    it('returns array of numbers 1 to 10 when easy is selected', () => {
+        const generatedNumber = generateNumberRange({ start: 1, end: 10 });
+        expect(generatedNumber).toStrictEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+    });
+
+    it('returns array of numbers 11 to 20 when moderate is selected', () => {
+        const generatedNumber = generateNumberRange({ start: 11, end: 20 });
+        expect(generatedNumber).toStrictEqual([
+            11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+        ]);
+    });
+
+    it('returns array of numbers 21 to 30 when hard is selected', () => {
+        const generatedNumber = generateNumberRange({ start: 21, end: 30 });
+        expect(generatedNumber).toStrictEqual([
+            21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+        ]);
+    });
 });
